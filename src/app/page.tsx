c30e71daf46f7a4075e9ec5996899985b35686c7
Edwin Hernández <edwinhernandez.br@gmail.com>
2023-08-28 21:54:35 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,142 @@
-import Image from 'next/image'
+"use client";
+
+import { FormEvent, useEffect, useState } from "react";
+
+interface IReto {
+  name: string;
+  date: string;
+  check: boolean;
+}
+
+interface IError {
+  state: boolean;
+  message: string;
+}
+
+const INITIAL_ERROR: IError = {
+  state: false,
+  message: ""
+}
 
 export default function Home() {
+
+  const [retos, setRetos] = useState<IReto[]>([]);
+  const [name, setName] = useState("");
+  const [error, setError] = useState(INITIAL_ERROR);
+
+  console.log(retos)
+
+  useEffect(() => {
+
+    const getRetos = JSON.parse(localStorage.getItem("retos")!);
+
+    if (!getRetos) {
+      localStorage.setItem("retos", JSON.stringify([]));
+    }
+    setRetos(getRetos);
+
+  }, []);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // validando campo
+    if (name === "") {
+      setError({
+        state: true,
+        message: "El nombre es obligatorio"
+      });
+
+      setTimeout(() => {
+        setError(INITIAL_ERROR);
+      }, 3000);
+      return;
+    }
+
+
+    const hoy = new Date();
+    const ahora = hoy.toLocaleString();
+
+    const nuevoReto: IReto = {
+      name: name,
+      date: ahora,
+      check: false
+    }
+
+    localStorage.setItem("retos", JSON.stringify([...retos, nuevoReto]));
+    setRetos([...retos, nuevoReto]);
+  }
+
+  const updateCheck = (index: number) => {
+    const updateCheckBox = retos.map((reto, i) => {
+      if (i === index) {
+        return { ...reto, check: !reto.check };
+      }
+      return reto;
+    });
+    setRetos(updateCheckBox);
+    localStorage.setItem("retos", JSON.stringify(updateCheckBox));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen h-full">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="container mx-auto px-4">
+        <section className="flex flex-col justify-center items-center h-full">
+          <h1 className="text-center text-zinc-200 text-4xl font-semibold mb-10">Retos diarios</h1>
+          <div className="flex justify-center w-full gap-6">
+            <section className="max-w-md w-full h-max border border-zinc-800 py-6 px-4 lg:px-10 rounded-xl">
+              <h2 className="text-zinc-200 text-2xl mb-4">Crear reto</h2>
+              <form onSubmit={handleSubmit}>
+                <article className="mb-10">
+                  <label
+                    htmlFor="name"
+                    className="block text-zinc-400 mb-2"
+                  >Nombre</label>
+                  <input
+                    id="name"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Crear carrusel de imagenes"
+                    className="w-full px-4 py-2 rounded-lg bg-zinc-800"
+                  />
+                </article>
+                <button
+                  className="px-4 py-3 w-full uppercase font-medium bg-emerald-500 hover:bg-emerald-400/90 rounded-lg"
+                  type="submit"
+                >
+                  Crear reto
+                </button>
+              </form>
+            </section>
+            <section className="flex flex-col gap-2 max-w-lg w-full h-max border border-zinc-800 py-6 px-4 lg:px-10 rounded-xl">
+              {
+                retos.length > 0 ? (
+                  retos.map((reto, index) => (
+                    <article key={index} className="flex justify-between items-center gap-4 w-full border border-zinc-800 rounded-lg p-4 hover:bg-zinc-950 select-none">
+                      <div>
+                        <p className="text-zinc-500 text-sm">{reto.date}</p>
+                        <p className="text-zinc-300 font-medium">{reto.name}</p>
+                      </div>
+                      <div className="w-10 h-10 border border-zinc-800 rounded-lg flex justify-center items-center">
+                        <input
+                          type="checkbox"
+                          className="accent-emerald-500"
+                          checked={reto.check}
+                          onChange={() => updateCheck(index)}
+                        />
+                      </div>
+                    </article>
+                  ))
+                ) : (
+                  <p className="text-zinc-300">No hay retos creados</p>
+                )
+              }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            </section>
+          </div>
+        </section>
       </div>
     </main>
   )
